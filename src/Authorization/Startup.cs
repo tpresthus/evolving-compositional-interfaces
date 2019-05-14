631de6e5c8faaba0d5a35d940740113f9edc7a55
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2019-05-14 23:29:40 +0200

    Renamed AuthenticatedUser to Authorization

--- src/Authorization/Startup.cs
+++ src/Authorization/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace AuthenticatedUser
+namespace Authorization
 {
     public class Startup
     {
@@ -27,7 +27,7 @@ namespace AuthenticatedUser
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("AuthenticatedUser");
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
