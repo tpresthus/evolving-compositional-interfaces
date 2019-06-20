e3bf5f7df08e31445edace70d7adc6bc488b92d9
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2019-06-21 01:06:30 +0200

    Remove cards and flippers

--- src/Admin/wwwroot/js/site.js
+++ src/Admin/wwwroot/js/site.js
@@ -2,20 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-document.querySelectorAll('.flipper').forEach(function(flipper) {
-    flipper.addEventListener('click', function(e) {
-        var node = this;
-
-        while (!node.classList.contains('flip-card')) {
-            node = node.parentNode;
-
-            if (!node.classList) {
-                console.error('No element with class "flip-card" found.');
-                return;
-            }
-        }
-
-        node.classList.toggle('is-flipped');
-    });
-});
