d29ce3de6f2d3485266ad0838f5598e312c990a8
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2019-06-21 03:10:48 +0200

    Map URIs to sensible names

--- src/Admin/Customers/CustomerViewModel.cs
+++ src/Admin/Customers/CustomerViewModel.cs
@@ -9,9 +9,9 @@ using Admin.Transactions;
 
 namespace Admin.Customers
 {
-    public class CustomerViewModel : BaseViewModel, IEnumerable<KeyValuePair<Uri, object>>
+    public class CustomerViewModel : BaseViewModel, IEnumerable<KeyValuePair<string, object>>
     {
-        private readonly LinkedDataObject customer;
+        private readonly IDictionary<string, object> data;
         
         public CustomerViewModel(LinkedDataObject customer, Menu menu, User user, UrlFactory urlFactory, IEnumerable<Transaction> transactions)
             : this(customer, menu, user, urlFactory)
@@ -33,26 +33,88 @@ namespace Admin.Customers
             }
 
             Id = ((Uri)customer["http://www.w3.org/ns/json-ld#id"]).PathAndQuery.TrimStart('/');
-            this.customer = customer;
+            Name = (string)customer["https://schema.org/name"];
+            this.data = new Dictionary<string, object>(Map(customer));
         }
 
         public string Id { get; }
+        public string Name { get; }
         public IEnumerable<TransactionViewModel> Transactions { get; }
-        public object this[string key] { get => customer[key]; set => customer[key] = value; }
+        public object this[string key] { get => data[key]; set => data[key] = value; }
+
+        private static IEnumerable<KeyValuePair<string, object>> Map(LinkedDataObject data)
+        {
+            foreach (var kvp in data)
+            {
+                var key = Map(kvp.Key);
+                if (key == null)
+                {
+                    continue;
+                }
+                
+                yield return new KeyValuePair<string, object>(key, kvp.Value);
+            }
+        }
+
+        private static string Map(Uri uri)
+        {
+            var uriString = uri.ToString();
+
+            switch (uriString)
+            {
+                case "https://schema.org/name":
+                    return "Name";
+
+                case "https://schema.org/PostalAddress":
+                    return "Address";
+                
+                case "https://schema.org/streetAddress":
+                    return "Street";
+                
+                case "https://schema.org/addressLocality":
+                    return "City";
+                
+                case "https://schema.org/postalCode":
+                    return "Zip code";
+                
+                case "https://schema.org/addressRegion":
+                    return "State";
+                
+                case "https://schema.org/telephone":
+                    return "Phone";
+                
+                case "https://schema.org/globalLocationNumber":
+                    return "Ssn";
+                
+                case "https://schema.org/birthDate":
+                    return "Born";
+                
+                case "https://schema.org/email":
+                    return "Email";
+                
+                case "https://schema.org/alternateName":
+                    return "UserName";
+                
+                case "https://schema.org/WebSite":
+                    return "Website";
+            }
+
+            return null;
+        }
 
         private TransactionViewModel MapTransaction(Transaction transaction)
         {
             return new TransactionViewModel(transaction);
         }
 
-        public IEnumerator<KeyValuePair<Uri, object>> GetEnumerator()
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
         {
-            return ((IDictionary<Uri, object>)customer).GetEnumerator();
+            return ((IDictionary<string, object>)data).GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return ((IDictionary<Uri, object>)customer).GetEnumerator();
+            return ((IDictionary<string, object>)data).GetEnumerator();
         }
     }
 }
